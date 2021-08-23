dad91f9f3c15ebe1c46fe26636b8b9787557a74c
unknown <kothamanoj288@gmail.com>
2021-08-23 17:17:23 +0530

    first project

---
+++ success/src/com/app/Test.java
@@ -0,0 +1,16 @@
+package com.app;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi this is manoj");
+		System.out.println(anothermethod());
+	}
+	
+	
+	static int anothermethod(){
+		int n=20;
+		return n;
+	}
+
+}
